a364f6619cae2769db732f3c8e84b64e0bd91a19
David Craig <david@silverstripe.com>
2014-11-25 21:21:09 +1300

    Fix initial loading issue

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -4,17 +4,20 @@ var React = require('react'),
     Palette = require('./palette'),
     Workspace = require('./workspace');
 
-var rootElement = document.getElementById('neolayout');
+window.jQuery('#neolayout').entwine({
+    onmatch: function () {
+        var rootElement = document.getElementById('neolayout'),
+            metadata = JSON.parse(rootElement.getAttribute('data-metadata')),
+            workspaceData = JSON.parse(rootElement.getAttribute('data-workspace'));
 
-var metadata = JSON.parse(rootElement.getAttribute('data-metadata')),
-    workspaceData = JSON.parse(rootElement.getAttribute('data-workspace'));
+        React.render(
+            <Palette metadata={metadata} />,
+            document.getElementById('palette')
+        );
 
-React.render(
-    <Palette metadata={metadata} />,
-    document.getElementById('palette')
-);
-
-React.render(
-    <Workspace data={workspaceData} metadata={metadata} />,
-    document.getElementById('workspace')
-);
+        React.render(
+            <Workspace data={workspaceData} metadata={metadata} />,
+            document.getElementById('workspace')
+        );
+    }
+});
