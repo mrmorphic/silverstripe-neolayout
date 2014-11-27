3677a8ed5f4c21f5c34c4850b1ee0599201efc5f
David Craig <david@silverstripe.com>
2014-11-27 20:13:56 +1300

    Allocate id's to fields that don't have them

--- jsx/workspace.jsx
+++ jsx/workspace.jsx
@@ -6,6 +6,7 @@
 'use strict';
 
 var React = require('react'),
+    uuid = require('node-uuid'),
     WorkspaceField = require('./workspaceField');
 
 var Workspace = React.createClass({
@@ -17,7 +18,9 @@ var Workspace = React.createClass({
 
     getInitialState: function () {
         return {
-            fieldData: this.props.data
+            // We're cloning this.props.data here because we can't tell if this.props.data
+            // needs to remain consistant further up the chain.
+            fieldData: this.allocateIds(JSON.parse(JSON.stringify(this.props.data)))
         };
     },
 
@@ -29,6 +32,26 @@ var Workspace = React.createClass({
     updateFieldData: function (value) {
         console.log('field data updated');
         console.log(value);
+    },
+
+    /**
+     * @func allocateIds
+     * @param {Object} workspaceField The field you wish to assign an id to.
+     * @return {Object} The updated workspaceField.
+     * @desc Recursivly allocate id's to a WorkspaceField. WorkspaceField's with an existing id are ignored.
+     */
+    allocateIds: function (workspaceField) {
+        var i = 0;
+
+        workspaceField.id = workspaceField.id || uuid.v4();
+
+        if (typeof workspaceField.children !== 'undefined' && workspaceField.children.length > 0) {
+            for (i; i < workspaceField.children.length; i += 1) {
+                workspaceField.children[i] = this.allocateIds(workspaceField.children[i]);
+            }
+        }
+
+        return workspaceField;
     },
 
     render: function () {
